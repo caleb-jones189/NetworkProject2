752a9fcbb5bb32351dda828a976cfc57aca0b51c
jonesc189 <caleb.jones@bobcats.gcsu.edu>
2018-09-11 16:56:13 -0400

    second commit

--- udpknockknockclient.c
+++ udpknockknockclient.c
@@ -18,7 +18,8 @@ int main(int argc, char **argv)
     struct hostent *hp;
     struct sockaddr_in server_addr, from_addr;
     socklen_t from_len;
-    char buf[MAX_LEN];
+    //char buf[MAX_LEN]={'K','n','o','c','k','!',' ','K','n','o','c','k','!','\0'} ;
+    char buf[MAX_LEN]="Knock! Knock!";
     int num_bytes;
     
     if (argc != 3) {
@@ -47,13 +48,13 @@ int main(int argc, char **argv)
     server_addr.sin_port = htons(atoi(argv[2]));
     
     // ask user for a message to be sent
-    /*printf("Please enter a message: ");
+    //printf("Please enter a message: ");
     memset(buf, 0, MAX_LEN);
-    fgets(buf, MAX_LEN, stdin);
-    */
-    buf="Knock! Knock!"
+    char buf[MAX_LEN]={'K','n','o','c','k','!',' ','K','n','o','c','k','!','\0'} ;
+    //fgets(buf, MAX_LEN, stdin);
     
-    // send message
+        
+    // send message KNOCK KNOCK 
     num_bytes = sendto(sock, buf, strlen(buf), 0,
                        (struct sockaddr *)&server_addr,
                        sizeof(server_addr));
@@ -62,7 +63,8 @@ int main(int argc, char **argv)
         exit(3);
     }
     
-    // receive message
+    memset(buf, 0, MAX_LEN);                                //added
+    // receive message WHOSE THERE
     from_len = sizeof(from_addr);   // must set (in & out)
     num_bytes = recvfrom(sock, buf, MAX_LEN, 0,
                          (struct sockaddr *)&from_addr,
@@ -71,12 +73,57 @@ int main(int argc, char **argv)
         perror("recvfrom");
         exit(4);
     }
- 
+
+     // display received ack message
+    //printf("Got an ack: ");
+
+    //PRINT WHOSE THERE
+    write(1, "Got an ack: ", 12);
+    write(1, buf, num_bytes);
+    printf("\n");
+    
+    memset(buf, 0, MAX_LEN);
+    char buf[MAX_LEN]={'R','o','b','i','n','\0'} ;
+
+    
+    // send message ROBIN 
+    num_bytes = sendto(sock, buf, strlen(buf), 0,
+                       (struct sockaddr *)&server_addr,
+                       sizeof(server_addr));
+    if (num_bytes < 0) {
+        perror("sendto");
+        exit(3);
+    }
+
+    memset(buf, 0, MAX_LEN);                        //added
+    // receive message ROBIN WHO?
+    from_len = sizeof(from_addr);   // must set (in & out)
+    num_bytes = recvfrom(sock, buf, MAX_LEN, 0,
+                         (struct sockaddr *)&from_addr,
+                         &from_len);
+    if (num_bytes < 0) {
+        perror("recvfrom");
+        exit(4);
+    }
+
+    //PRINT ROBIN WHO 
     // display received ack message
     //printf("Got an ack: ");
     write(1, "Got an ack: ", 12);
     write(1, buf, num_bytes);
     printf("\n");
+
+        memset(buf, 0, MAX_LEN);                    //added
+        char buf[MAX_LEN]={'R','o','b','i','n',' ','y','o','u',' ','.','H','a','n','d',' ','o','v','e','r',' ','y','o','u','r',' ','c','a','s','h','.','\0'} ;
+     // send message ROBIN YOU... 
+    num_bytes = sendto(sock, buf, strlen(buf), 0,
+                       (struct sockaddr *)&server_addr,
+                       sizeof(server_addr));
+    if (num_bytes < 0) {
+        perror("sendto");
+        exit(3);
+    }
+    
     
     // close sock to release resource
     close(sock);
